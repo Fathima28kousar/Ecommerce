015842182a00b385ed20838ad77a1a4a341921af
Fathima28kousar <kousar28fathima@gmail.com>
2024-03-01 14:07:05 +0530

    updated cart page

--- ecommerce/src/components/checkout/Checkout.jsx
+++ ecommerce/src/components/checkout/Checkout.jsx
@@ -1,31 +1,42 @@
-import styles from './Checkout.module.css'
+import styles from "./Checkout.module.css";
+import Form from "./Form";
 
 const Checkout = () => {
   return (
     <div className={styles.container}>
-        <div className={styles.checkout}>
-            <h1>Checkout</h1>
-
-            <div className={styles.billingDetails}>
-                <div className={styles.form}>
-                    <form>
-                        <div>
-                            <label>First Name</label>
-                            <input type='text' />
-                        </div>
-
-                        <div>
-                            <label>Last Name</label>
-                            <input type='text' />
-                        </div>
-                        
-                    </form>
-                </div>
-                <div className={styles.order}></div>
-            </div>
+      <div className={styles.checkout}>
+        <h1>Checkout</h1>
+        <div className={styles.billingDetails}>
+          <div className={styles.form}>
+            <h5>Billing details</h5>
+            <hr />
+            <Form />
+          </div>
+          <div className={styles.order}>
+            <h1>Your order</h1>
+            <table>
+              <tr>
+                <th>Product</th>
+                <th>SubTotal</th>
+              </tr>
+              <tr>
+                <td>Assorted coffee * 1</td>
+                <td>35</td>
+              </tr>
+              <tr>
+                <td>SubTotal</td>
+                <td>35</td>
+              </tr>
+              <tr>
+                <td>Total</td>
+                <td>35</td>
+              </tr>
+            </table>
+          </div>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Checkout
+export default Checkout;
